409e1a79fb3632e8e9eeb753830999a62659b242
Shravya-96 <45030589+Shravya-96@users.noreply.github.com>
2020-09-17 14:55:52 +0530

    first commit

---
+++ Rest/src/main/java/com/test/RestfulXMLExample.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+ 
+@Path("/customers")
+public class RestfulXMLExample {
+ 
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Customer getCustomerDetails(@PathParam("id") int custId) {
+        
+        
+     // WRITE DATABASE LOGIC TO RETRIEVE THE CUSTOMER RECORD WITH 'custID'
+        
+        Customer cust = new Customer();
+        cust.setCustName("Java4s");
+        cust.setCustCountry("USA");
+        cust.setCustNo(custId);
+ 
+        return cust;        
+     }
+}
